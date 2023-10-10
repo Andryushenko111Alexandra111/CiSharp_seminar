c862ce8671d895e355797a3bfd6e346b34c09639
[AlexAlexAndra78] <[Alexandra28.78@mail.ru]>
2023-10-10 19:08:36 +0300

    решение 2 дз

---
+++ task15_Является_ли_день_недели_выходным/Program.cs
@@ -0,0 +1,33 @@
+﻿/*
+Напишите программу, которая принимает на 
+вход цифру, обозначающую день недели, и проверяет, 
+является ли этот день выходным.
+6 -> да
+7 -> да
+1 -> нет
+*/
+int Prompt(string message) //создали функцию (тип название)
+{
+    System.Console.WriteLine(message);
+    string inputString = Console. ReadLine();//ожидание ввода строки и присваивание её значения
+    int number = Convert.ToInt32(inputString);
+    return number;// возврат числа и выход из функции
+}
+
+int day = Prompt("Введите номер дня недели: ");// объявление переменной day, присваеваем результат вызова функции Prompt
+
+if (day > 7)
+{
+   System.Console.WriteLine("Ведённое число слишком большое."); 
+}
+else
+{
+   if (day <6)
+   {
+      System.Console.WriteLine("нет");
+   }
+   else
+   {
+      System.Console.WriteLine("да");
+   }
+}
